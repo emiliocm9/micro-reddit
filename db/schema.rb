7434e797ab2d6ade8735b551f405a0ce4bde8868
brugobi <brgobbi@hotmail.com>
2020-07-22 17:23:33 -0400

    created a model for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_202102) do
+ActiveRecord::Schema.define(version: 2020_07_22_212244) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "author"
